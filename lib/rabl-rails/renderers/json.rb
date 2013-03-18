f28a4b9e114bb0b63e81deda5cf0c69afb8d9778
ccocchi <cocchi.c@gmail.com>
2013-03-18 19:42:44 +0100

    First draft of caching

--- lib/rabl-rails/renderers/json.rb
+++ lib/rabl-rails/renderers/json.rb
@@ -5,6 +5,10 @@ module RablRails
         hash = { _options[:root_name] => hash } if _options[:root_name] && RablRails.include_json_root
         MultiJson.encode(hash)
       end
+
+      def resolve_cache_key(data, key)
+        "#{super}.json"
+      end
     end
   end
 end
